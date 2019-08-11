cba5079e3edafe54b14353a5bec52feecfd34410
Re-Coma <seokbong60@gmail.com>
2019-08-11 18:35:39 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -6,17 +6,6 @@ using namespace std;
 using namespace structure;
 int main(void) {
 
-	IsInStringRange isInStringRange;
-
-	DataElement strEle("HiTension", DATATYPE_STRING, STRUCTTYPE_ELEMENT);
-	DataElement testNum("100", DATATYPE_NUMBER, STRUCTTYPE_ELEMENT);
-
-	ScanDataCondition condition("*Tenas*");
-
-	if( isInStringRange(condition, strEle) )
-		cout << "yes!" << endl;
-	else
-		cout << "no!" << endl;
 
 	return 0;
 }
