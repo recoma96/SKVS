809097485243d482d41923351c3fd621cca79f80
Re-Coma <seokbong60@gmail.com>
2019-08-11 18:35:11 +0900

    탐색/삭제 함수 구현 및 초안 마무리

--- lib/structure/Set.cpp
+++ lib/structure/Set.cpp
@@ -41,33 +41,83 @@ void Set::updateLength(void) noexcept {
 
 
 //조건에 따른 Set안의 데이터 수집
+//!! deleteRange에서 이 함수를 사용하기 위해 list의 Element는 원소 자체가 아닌 포인터
+//결과데이터의 복사본 출력은 DataBase Class에서 구현
 list<DataElement*> Set::searchRange(ScanDataCondition& _condition) {
 	
 	list<DataElement*> resultList;
 
 	if( this->valueSet.empty() ) return resultList;
 
-	bool isSearchByString = false;
-	// 범위값으로 탐색하는지 아니면
-	// 문자열로 찾을 것인지 판별:
-		
-	//탐색
-	for( set<DataElement*>::iterator iter = this->valueSet.begin();
-			iter != this->valueSet.end(); iter++ ) {
 
+	//ConditionClass의 내용이 비었음 -> 전체범위
+	if( _condition.isEmpty() ) {
+		for( set<DataElement*>::iterator iter = this->valueSet.begin();
+				iter != this->valueSet.end(); iter++ ){
+			resultList.push_back( (*iter) );
+		}
+		return resultList;
+	} else {
+
+		//number,float와 string으로 탐색알고리즘 스타일이 바뀜
+		//MultiDataStructure.hpp
+
+		//1. string
+		if( this->dataType == DATATYPE_STRING ) {
+			
+			//condition첫부분이 string이 아니면 DataTypeException 반환
+			if( _condition.conditionToken[0]->dcf != DCF_STRING ) 
+				throw DataTypeException("This Set type is string but condition style is not string.");
+			else {
+				//순환하면서 탐색
+				IsInStringRange isInStringRange;
+
+				for( set<DataElement*>::iterator iter = this->valueSet.begin();
+						iter != this->valueSet.end(); iter++ ) {
+					
+					if(isInStringRange(_condition, **iter))
+						resultList.push_back(*iter);
+				}
+			}
+		} else { // == DATATYPE_NUMBER, DATATYPE_FLOAT
+			
+			if(_condition.conditionToken[0]->dcf == DCF_STRING )
+				throw DataTypeException("This Set type is numberical but, condition style is string.");
+			else {
+				
+				IsInNumberRange isInNumberRange;
+
+				for(set<DataElement*>::iterator iter = this->valueSet.begin();
+						iter != this->valueSet.end(); iter++ ) {
+
+					if( isInNumberRange(_condition, **iter))
+						resultList.push_back(*iter);
+				}
+			}
+		}
 
 	}
-
+		
 	return resultList;
 }
 
 //조건에 따른 데이터 삭제
 bool Set::deleteRange(ScanDataCondition& _condition) {
 
+	//데이터 탐색
+	list<DataElement*> deleteList = searchRange(_condition);
+	if( deleteList.empty() ) return false; //삭제 실패
+
+	//차례대로 제거
+	for( list<DataElement*>::iterator iter = deleteList.begin();
+			iter != deleteList.end(); iter++ ) {
+		this->valueSet.erase(*iter);
+	}
+
 	//마지막에 데이터 갯수 업데이트
 	this->updateLength();
 
-	return false;
+	return true;
 }
 
 //데이터 사이즈 구하기
