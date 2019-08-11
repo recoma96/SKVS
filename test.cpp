2dbf889376cab26026b26b2ab1cd0ea3eb8bc74b
Re-Coma <seokbong60@gmail.com>
2019-08-11 17:35:15 +0900

    테스트용ㅋ

--- test.cpp
+++ test.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-#include "lib/structure/Set.hpp"
+#include "lib/structure/MultiDataStructure.hpp"
 
 using namespace std;
 
 using namespace structure;
 int main(void) {
 
-	DataElement* a = nullptr;
+	IsInStringRange isInStringRange;
+
+	DataElement strEle("HiTension", DATATYPE_STRING, STRUCTTYPE_ELEMENT);
+	DataElement testNum("100", DATATYPE_NUMBER, STRUCTTYPE_ELEMENT);
+
+	ScanDataCondition condition("*Tenas*");
+
+	if( isInStringRange(condition, strEle) )
+		cout << "yes!" << endl;
+	else
+		cout << "no!" << endl;
 
 	return 0;
 }
