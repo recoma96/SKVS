37ae57ddfcfc12a75108d07db2fc1e7d7016a907
Re-Coma <seokbong60@gmail.com>
2019-08-15 10:14:00 +0900

    hashmap abstract class

---
+++ lib/structure/HashMapLayer.hpp
@@ -0,0 +1,27 @@
+/***
+ *  @file : HashMapLayer.hpp
+ * 	@date 2019/08/15
+ *  @author 하정현(seokbong60@gmail.com) Team:SweetCase Project (1-person Team)
+ *  @brief : HashMap 추상화 클래스
+ * 
+ ***/
+
+#ifndef HASHMAPLAYER_HPP
+# define HASHMAPLAYER_HPP
+
+#include "HashMap.hpp"
+
+namespace structure {
+    class HashMapLayer : public HashMap {
+    public:
+        HashMapLayer(const string _data, const StructType _structType ) :
+            HashMap(_data, _structType ) { }
+        
+        const bool insertKeyValue(const string _newKey, const string _newValue);
+        const bool updateKey( const string _targetKey, const string _newKey);
+        const bool updateValue(const string _targetKey, const string _newValue);
+        
+    };
+}
+
+#endif
