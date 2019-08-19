50b82ddce043e50e9519ac0b64de122059b3deb0
Re-Coma <seokbong60@gmail.com>
2019-08-19 22:26:12 +0900

    로그 서버에 사용할 클래스 초안

---
+++ lib/logbase/LogBase.cpp
@@ -0,0 +1,44 @@
+#include "LogBase.hpp"
+#include <sys/types.h>
+#include <dirent.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+LogBase::LogBase(const string _dirRoot, shared_ptr<ThreadAdapter::AdapterThreadUtility>& _adapterQueue) {
+    dirRoot = _dirRoot;
+    adapterQueue = _adapterQueue;
+}
+
+//fileRoot의 폴더가 존재하지 않으면 false 반환
+// log_file : fileRoot/20XX_XX_XX.log
+const bool LogBase::writeLogToFile(LogPacket* _logPacket) {
+
+    //디렉토리 유무 확인
+    DIR* dp = nullptr;
+    cout << dirRoot << endl;
+    //없을 경우 false 반환
+    if( (dp=opendir(dirRoot.c_str())) == nullptr) {
+        return false;
+    }
+
+    //디렉토리 닫기
+    //closedir(dp);
+
+    //오늘 날짜 갖고오기
+    string todayLogFile = CalTime::makeLogDataFileName();
+
+    string totalFileRoot;
+    totalFileRoot += dirRoot;
+    totalFileRoot += todayLogFile;
+
+    //로그 작성
+    ofstream logFile(totalFileRoot, ios::out | ios::app);
+    logFile << _logPacket->getStatement() << endl;
+    logFile.close();
+    
+    //delete _logPacket;
+    return true;
+}
---
+++ lib/logbase/LogBase.hpp
@@ -0,0 +1,25 @@
+/***
+ *  @file : CommandList.hpp
+ * 	@date 2019/08/19
+ *  @author 하정현(seokbong60@gmail.com) Team:SweetCase Project (1-person Team)
+ *  @brief : 로그데이터를 관리합니다.
+ * 
+ ***/
+#include <string>
+#include "../threadAdapter/AdapterThreadUtility.hpp"
+#include "../packet/Packet.hpp"
+
+using namespace std;
+using namespace ThreadAdapter;
+
+//AdpaterThread를 필요로 하는 클래스 입니다.
+// log_file : fileRoot/20XX_XX_XX.log
+class LogBase {
+private:
+    string dirRoot;
+    weak_ptr<AdapterThreadUtility> adapterQueue;
+public:
+    LogBase(const string _fileRoot, shared_ptr<ThreadAdapter::AdapterThreadUtility>& _adapterQueue);
+    const bool writeLogToFile(LogPacket* _logPacket);
+
+};
