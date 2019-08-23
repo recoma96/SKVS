9dc23376eec17bd3301c389d78933ddaefd0ca7b
Re-Coma <seokbong60@gmail.com>
2019-08-23 10:45:03 +0900

    sort 기능 추가

--- lib/structure/List.hpp
+++ lib/structure/List.hpp
@@ -19,6 +19,44 @@
 
 namespace structure {
 
+    //리스트를 sort할 때 사용하는 객체함수
+    struct SortStringListBackward {
+        bool operator()( shared_ptr<DataElement> first, shared_ptr<DataElement> second ) {
+            return (first->getDataToString().compare(second->getDataToString()) > 0);
+        }
+    };
+    struct SortNumberListBackward {
+        bool operator() ( shared_ptr<DataElement> first, shared_ptr<DataElement> second ) {
+            return first->getData<number_t>() > second->getData<number_t>();
+        }
+    };
+    struct SortFloatListBackward {
+        bool operator() ( shared_ptr<DataElement> first, shared_ptr<DataElement> second ) {
+            return first->getData<float_t>() > second->getData<float_t>();
+        }
+    };
+    struct SortStringListForward {
+        bool operator()( shared_ptr<DataElement> first, shared_ptr<DataElement> second ) {
+            return (first->getDataToString().compare(second->getDataToString()) < 0);
+        }
+    };
+    struct SortNumberListForward {
+        bool operator() ( shared_ptr<DataElement> first, shared_ptr<DataElement> second ) {
+            return first->getData<number_t>() < second->getData<number_t>();
+        }
+    };
+    struct SortFloatListForward {
+        bool operator() ( shared_ptr<DataElement> first, shared_ptr<DataElement> second ) {
+            return first->getData<float_t>() < second->getData<float_t>();
+        }
+    };
+
+    //sort 플래그
+    enum SortFlag {
+        SORTFLAG_ASEN,
+        SORTFLAG_DESN
+    };
+
     class List : public MultiDataStructureLayer {
     protected:
         list<shared_ptr<DataElement>> valueList;
@@ -48,6 +86,36 @@ namespace structure {
             for(list<shared_ptr<DataElement>>::iterator iter = this->valueList.begin(); 
                 iter != this->valueList.end(); iter++ ) {
                 cout << (*iter)->getDataToString() << endl;
+            }
+        }
+
+        void sort(SortFlag sortFlag) {
+            if(!valueList.empty()) {
+                if(sortFlag == SORTFLAG_ASEN) { //오름차순
+                    switch( (*(valueList.begin()))->getDataType()) {
+                        case DATATYPE_STRING:
+                            valueList.sort(SortStringListForward());
+                        break;
+                        case DATATYPE_NUMBER:
+                            valueList.sort(SortNumberListForward());
+                        break;
+                        case DATATYPE_FLOAT:
+                            valueList.sort(SortFloatListForward());
+                        break;
+                    }
+                } else { //내림차순
+                    switch((*(valueList.begin()))->getDataType()) {
+                        case DATATYPE_STRING:
+                            valueList.sort(SortStringListBackward());
+                        break;
+                        case DATATYPE_NUMBER:
+                            valueList.sort(SortNumberListBackward());
+                        break;
+                        case DATATYPE_FLOAT:
+                            valueList.sort(SortFloatListBackward());
+                        break;
+                    }
+                }
             }
         }
     };
